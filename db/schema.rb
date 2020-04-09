0b6524df6caa9612dd6f77a9dcd07b362f4aef55
Kouki Kyoguro <kyogurokouki@Kouki-KyoguronoMacBook-Pro.local>
2020-04-09 14:48:51 +0900

    added user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_094713) do
+ActiveRecord::Schema.define(version: 2020_04_09_043352) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2020_04_08_094713) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
